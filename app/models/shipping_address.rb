4cb599cca74ce4b12ce9cda2f9ea6b878cd1c67c
Alexander Leishman <leishman3@gmail.com>
2014-12-01 17:39:06 -0800

    add some more basic styling and order confirmation

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,4 @@
 class ShippingAddress < ActiveRecord::Base
   belongs_to :order
-  validates :address_line_1, :address_line_2, :city, :state, :zip_code, presence: true
+  validates :address_line_1, :city, :state, :zip_code, presence: true
 end
--- spec/models/shipping_address_spec.rb
+++ spec/models/shipping_address_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe ShippingAddress do
-  attrs = [:address_line_1, :address_line_2, :city, :state, :zip_code]
+  attrs = [:address_line_1, :city, :state, :zip_code]
   attrs.each do |a|
     it { should validate_presence_of a }
   end
