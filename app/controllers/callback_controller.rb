ba851c5ebc7c2f3ac0e2c1a6dc72f3c2c53f216b
Alexander Leishman <leishman3@gmail.com>
2014-12-02 21:58:34 -0800

    update callback controller

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,8 +2,11 @@ class CallbackController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def callback
-    order = Order.find_by(uid: params[:checkout][:merchant_ref_id])
-    order.status = params[:checkout][:status]
-    order.save!
+    if order = Order.find_by(secret_id: params[:checkout][:merchant_ref_id])
+      order.status = params[:checkout][:status]
+      order.save!
+    else
+      raise 'could not find order'
+    end
   end
 end
