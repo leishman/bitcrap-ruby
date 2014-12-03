7584a81e4dd1ad0b2a9aef939393ec305c56a68b
Alexander Leishman <leishman3@gmail.com>
2014-12-02 21:54:49 -0800

    skip verify auth token for callback controller

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,4 +1,6 @@
 class CallbackController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def callback
     order = Order.find_by(uid: params[:checkout][:merchant_ref_id])
     order.status = params[:checkout][:status]
