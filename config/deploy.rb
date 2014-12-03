a688ae5292b5d71c599616c7d413a02340cd4364
Alexander Leishman <leishman3@gmail.com>
2014-12-02 20:39:35 -0800

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 # lock '3.2.1'
 
 set :application, 'bitcrap'
-set :repo_url, 'https://github.com/leishman/bitcrap-ruby.git'
+set :repo_url, 'git@github.com:leishman/bitcrap-ruby.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
