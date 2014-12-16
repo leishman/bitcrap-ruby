af031fbd1f784aea4478e844736cc6a5b2bf9002
Alexander Leishman <leishman3@gmail.com>
2014-12-16 10:13:54 -0800

    add random tagline and dimensions for image to make rendering faster

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,19 @@ class HomeController < ApplicationController
   def index
     @order = Order.new
     @order.build_shipping_address
+    @tagline = get_tagline
+  end
+
+  private
+
+  def get_tagline
+    [
+      'Store of the Future',
+      'All Your Friends Are Doing It',
+      'Gifts, Made Thoughtless',
+      "Mo Money, Mo' Bitcrap",
+      'Young Money, Hash Money',
+      'Because Why Not?'
+    ].sample
   end
 end
